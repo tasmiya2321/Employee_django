ebdadd8057c6139f725455ba47f86787fc3f2d1a
priyadharsh2002 <rengarajpriyadarshini@gmail.com>
2023-11-27 17:34:24 +0530

    make changes

--- Emp_app/urls.py
+++ Emp_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
       # Uncomment the next line to enable the admin:
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', views.index,name="index"),
     path("", views.login, name="login"),
-    path("home/",views.home,name="home")
+    path("home/",views.home,name="home"),
+    path("",views.base,name="base")
 ]
