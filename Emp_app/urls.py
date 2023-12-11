bac32766beed18eb577ca0ceec927018ca9de90e
hariraj22 <harirajp2292003@gmail.com>
2023-12-11 07:01:15 -0800

    push

--- Emp_app/urls.py
+++ Emp_app/urls.py
@@ -1,18 +1,3 @@
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#       # Uncomment the next line to enable the admin:
-#     path('admin/', admin.site.urls),
-#     # path('', views.index,name="index"),
-#     # path("", views.login, name="login"),
-#     # path("home/",views.home,name="home"),
-       # path("employee/",views.employee,name="employee")
-
-
-
-
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -20,13 +5,13 @@ from . import views
 
 
 urlpatterns = [
-      # Uncomment the next line to enable the admin:
+     
     path('admin/', admin.site.urls),
-    # path("employee/", views.employee, name="employee"),
+    path("employee/", views.employee, name="employee"),
     path("home/",views.home,name="home"),
-    #path('', views.index,name="index"),
-    # path("", views.login, name="login"),
-    # path('forgetpassword/', views.forgetpassword, name='forget_password')
+    path('', views.index,name="index"),
+    path("", views.login, name="login"),
+    path('forgetpassword/', views.forgetpassword, name='forget_password')
 ]
 # if settings.DEBUG:
 #     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
--- Emp_app/views.py
+++ Emp_app/views.py
@@ -3,24 +3,41 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .models import Program 
+from .models import EmpDetails 
 
 def home(request):
-     programs = Program.objects.values('date', 'pgm_id', 'xref_id', 'activity', 'center_type', 'beneficiaries')
-     
-     return render(request, "Emp_app/home.html", {'programs': programs})
+    programs = Program.objects.exclude(date__isnull=True).order_by('pgm_id')[:10]
+    
+    # Debugging: Print each program's date
+    for program in programs:
+        print(program.date)
+
+    return render(request, "Emp_app/home.html", {'programs': programs})
 
 
 
-# def forgetpassword(request):
-#      return render(request, "Emp_app/forgetpassword.html")
+
+
+def forgetpassword(request):
+     return render(request, "Emp_app/forgetpassword.html")
 
     
-# def index(request):
-#     return render(request, "Emp_app/index.html")
+def index(request):
+    return render(request, "Emp_app/index.html")
 
+    
+def employee(request):
+    return render(request, "Emp_app/employee.html")
 
 # def employee(request):
-#      return render(request, "Emp_app/employee.html")
+#         # Query the EmpDetails model for all records
+#     emp_details = EmpDetails.objects.all()
+
+#     # Pass the queried data to the template
+#     context = {
+#         'emp_details': emp_details
+#     }
+#     return render(request, "Emp_app/employee.html", context)
 
 
 
