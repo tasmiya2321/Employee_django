2229d38e57b5143d1215c9a5dd05087853f8bfb5
hariraj22 <harirajp2292003@gmail.com>
2023-12-20 19:45:37 +0530

    email

--- Emp_app/urls.py
+++ Emp_app/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
-from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView
-from django.contrib.messages.views import SuccessMessageMixin
-class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'users/password_reset.html'
-    email_template_name = 'users/password_reset_email.html'
+#from . import ResetPasswordView
+from django.contrib.auth import views as auth_views
 
 
 
@@ -19,8 +14,14 @@ urlpatterns = [
     path('', views.index, name="index"),
     path("", views.login, name="login"),
     path('forgetpassword/', views.forgetpassword, name='forget_password'),  # Add a comma here
-    path('reset_password/', views.reset_password, name='reset_password')
+    path('reset_password/', views.ResetPasswordView.as_view(), name='reset_password'),
+    path('reset_password-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='reset_password_confirm.html'),
+         name='reset_password_confirm'),
+    path("email/", views.email, name="email"),
+    path("subject_email/", views.subject_email, name="subject_email")
 ]
+
 # if settings.DEBUG:
 #     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #     urlpatterns += staticfiles_urlpatterns()
--- Emp_app/views.py
+++ Emp_app/views.py
@@ -5,6 +5,23 @@ from django.contrib.auth import authenticate, login
 from .models import Program 
 from .models import EmpDetails 
 
+
+
+from django.urls import reverse_lazy
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'Emp_app/reset_password.html'
+    email_template_name = 'Emp_app/email.html'
+    subject_template_name ='Emp_app/subject_email'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = reverse_lazy('index')
+    
+
+
 def home(request):
     programs = Program.objects.exclude(date__isnull=True).order_by('pgm_id')[:10]
     
@@ -32,6 +49,12 @@ def employee(request):
 
 def reset_password(request):
      return render(request, "Emp_app/reset_password.html")
+
+def email(request):
+     return render(request, "Emp_app/email.html")
+
+def subject_email(request):
+     return render(request, "Emp_app/subject_email.txt")
 
 # def employee(request):
 #         # Query the EmpDetails model for all records
