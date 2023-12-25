27682c27dc4ff2a97c3ea015b0d60724dc812422
crazyambivert <smita.kasinath@gmail.com>
2023-12-25 09:54:22 +0530

    password reset

--- Emp_app/urls.py
+++ Emp_app/urls.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls import include
-
 from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
+
 # class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
 #     template_name = 'users/password_reset.html'
 #     email_template_name = 'users/password_reset_email.html'
--- Employee_Management/settings.py
+++ Employee_Management/settings.py
@@ -34,9 +34,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
    
     # Add your apps here to enable them
-    'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.admin',
+        'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Employee_Management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + "/" + "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
