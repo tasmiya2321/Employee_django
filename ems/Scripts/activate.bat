9d55e9f009c3fbf78aa6340dc21a306d0fba6673
tasmiya2321 <136069103+tasmiya2321@users.noreply.github.com>
2023-12-29 10:57:38 +0530

    commit

--- ems/Scripts/activate.bat
+++ ems/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\hp\source\repos\tasmiya2321\Employee_django\ems
+set VIRTUAL_ENV=C:\Users\hp\Source\Repos\tasmiya2321\Employee_django\ems
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(ems) 
 
 :END
 if defined _OLD_CODEPAGE (
