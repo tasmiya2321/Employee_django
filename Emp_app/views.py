fd62e8ff897d892616ed163fa28a13180bd6ca24
hp <rengarajpriyadarshini@gmail.com>
2023-12-30 17:41:06 +0530

    All pages

--- Emp_app/views.py
+++ Emp_app/views.py
@@ -154,7 +154,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('home')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
